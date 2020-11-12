f3b89cd6e169f6a02135f1d16a6f50921ae36e97
今吉恒介 <>
2020-11-12 19:24:46 +0900

    コンフリクト　解消

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,17 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-
+window.addEventListener('load', () => {//画像までの読み込みを行う
+  const uploader = document.querySelector('.file');
+  uploader.addEventListener('change', (e) => {//.fileの値の変更が確定された時
+    const file = uploader.files[0];// files[0] 選択したファイルオブジェクトを取得する
+    const reader = new FileReader();// FileReaderオブジェクトをインスタンス化.a1FileReaderはオブジェクトからデータを読み込むことのみを目的としたオブジェクト
+    reader.readAsDataURL(file);// readAsDataURL(file) file画像の読み込み。データを base64 データurl にエンコードします
+    reader.onload = () => {// onload 読み込みが成功したか調べる
+      const image = reader.result;// result readerの読み込み成功後に、中身のデータを取得する。（読み取り専用）
+      document.querySelector('.avatar').setAttribute('src', image);
+    }
+  });
+});
 
 
