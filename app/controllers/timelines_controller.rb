154e3dd39af024a09ccdd1bb69d3c17863f6c95e
今吉恒介 <>
2020-12-08 16:22:11 +0900

    24時間以内の投稿の中でいいねが多い投稿を５つ表示

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,8 +1,20 @@
 class TimelinesController < ApplicationController
   before_action :authenticate_user!
+  # module ActiveRecord
+  #   class Base
+  #     def all_columns
+  #       column_names.collect { |c| "#{table_name}.#{c}" }.join(",")
+  #     end
+  #   end
+  # end
   def show 
-    user_ids = current_user.followings.pluck(:id)
-    articles = Article.joins(:likes).group('likes.article.id')
-.order().limit(5)
-  end
+    #  Time.current
+    @articles = Article.includes(:likes).where(created_at: Time.current.ago(24.hours)..Time.current).where(id: Like.group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id))
+    # articles = Article.find(Like.group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id))
+    # if articles.where(created_at: from..to).exists?
+    #   @articles = articles.where(created_at: from..to)
+    # end
 end
+end
+# Article.includes(:likes).where(created_at: from..to).group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id))
+# Article.includes(:likes).where(created_at: Time.current.ago(24.hours)..Time.current)
